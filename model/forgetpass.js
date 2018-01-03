264c6ebcf760fb1b3ac4d1d59741916a41be9ed3
shivanidesai14 <shivanidesai402@gmail.com>
2018-01-03 10:02:52 +0530

    changes in service

---
+++ model/forgetpass.js
@@ -0,0 +1,24 @@
+var email = require("emailjs/email");
+var forgetPass = {
+
+    sendMail: function(demo, callback) {
+
+        var server = email.server.connect({
+            user: "expensetracker8@gmail.com",
+            password: "shivanidesai",
+            host: "smtp.gmail.com",
+            ssl: true,
+            port: 465
+        });
+
+        server.send({
+            text: demo.message,
+            from: "expensetracker8@gmail.com",
+            to: demo.name,
+            subject: demo.subject
+        }, callback);
+    }
+
+
+}
+module.exports = forgetPass;
--- model/tbl_expenses.js
+++ model/tbl_expenses.js
@@ -1,11 +1,12 @@
 var db = require('../connection');
 var expenses = {
     getAllExpenses: function(callback) {
-        return db.query("select e.*,s.sub_cat_name from exp_tbl e,sub_category_tbl s where e.fk_scat_id = s.sub_cat_id", callback);
+        return db.query("select e.*,s.sub_cat_name,s.icon_image from exp_tbl e,sub_category_tbl s where e.fk_scat_id = s.sub_cat_id", callback);
     },
     getExpensesById: function(id, callback) {
-        return db.query("select * from exp_tbl where fk_user_email=?", [id], callback)
+        return db.query("select e.*,s.sub_cat_name,s.icon_image  from exp_tbl e,sub_category_tbl s where e.fk_scat_id = s.sub_cat_id and e.fk_user_email=?", [id], callback)
     },
+
     addExpenses: function(exp, callback) {
         return db.query("Insert into exp_tbl values(?,?,?,?,?,?,?)", [null, exp.fk_user_email, exp.fk_scat_id, exp.expense_date, exp.expense_amt, exp.colour_name, exp.exp_note], callback);
     },
