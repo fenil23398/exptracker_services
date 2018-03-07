1e237c9a6181c866306278d36f858687980e0e2a
fenil23398 <fenilshah23398@gmail.com>
2018-03-07 18:33:40 +0530

    final changes for graph

--- model/chartdoughnut.js
+++ model/chartdoughnut.js
@@ -5,15 +5,15 @@ var chartd = {
     },
     getexps:function(id,mon,callback)
     {
-        return db.query("select category_tbl.cat_name,sum(exp_tbl.expense_amt) from category_tbl,exp_tbl,sub_category_tbl where category_tbl.cat_id=sub_category_tbl.fk_cat_id and sub_category_tbl.sub_cat_id=exp_tbl.fk_scat_id and (category_tbl.fk_user_email=? OR category_tbl.fk_user_email=?) and exp_tbl.exp_month=? GROUP BY category_tbl.cat_id",[id,'expensetracker8@gmail.com',mon],callback);
+        return db.query("select category_tbl.cat_name,sum(exp_tbl.expense_amt) from category_tbl,exp_tbl,sub_category_tbl where category_tbl.cat_id=sub_category_tbl.fk_cat_id AND sub_category_tbl.sub_cat_id=exp_tbl.fk_scat_id and exp_tbl.fk_user_email=? AND exp_tbl.exp_month=? GROUP BY category_tbl.cat_id",[id,mon],callback);
     },
     getexpforline:function(id,callback)
     {
-        return db.query("select sum(expense_amt),exp_month from exp_tbl where fk_user_email=? OR fk_user_email=? GROUP BY exp_month",[id,'expensetracker8@gmail.com'],callback);
+        return db.query("select sum(expense_amt),exp_month from exp_tbl where fk_user_email=? GROUP BY exp_month",[id],callback);
     },
     getexpforbar:function(id,callback)
     {
-        return db.query("select sum(exp_tbl.expense_amt),category_tbl.cat_name from category_tbl,exp_tbl,sub_category_tbl where category_tbl.cat_id=sub_category_tbl.fk_cat_id AND sub_category_tbl.sub_cat_id=exp_tbl.fk_scat_id AND (category_tbl.fk_user_email=? OR category_tbl.fk_user_email=?) AND (sub_category_tbl.s_fk_user_email=? OR sub_category_tbl.s_fk_user_email=?) GROUP BY category_tbl.cat_id ",[id,'expensetracker8@gmail.com',id,'expensetracker8@gmail.com'],callback); 
+        return db.query("select sum(exp_tbl.expense_amt),category_tbl.cat_name from category_tbl,exp_tbl,sub_category_tbl where category_tbl.cat_id=sub_category_tbl.fk_cat_id AND sub_category_tbl.sub_cat_id=exp_tbl.fk_scat_id AND exp_tbl.fk_user_email=? GROUP BY category_tbl.cat_id ",[id],callback); 
     }
     
     
